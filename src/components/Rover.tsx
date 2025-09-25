8d40c1bdd10ee62c0e74ae8a113209185542fa87
flxinxout <giovanni.ranieri@epfl.ch>
2025-09-25 14:34:33 +0200

    rover model phoenyx

--- src/components/Rover.tsx
+++ src/components/Rover.tsx
@@ -1,14 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF, Bounds } from "@react-three/drei";
 
-const baseUrl = import.meta.env.BASE_URL;
+interface Props {
+  name: string;
+  baseUrl: string;
+}
 
-function Model() {
-  const { scene } = useGLTF(`${baseUrl}/models/rover/rover.glb`);
+function Model({ name, baseUrl }: Props) {
+  const { scene } = useGLTF(`${baseUrl}/models/rover/${name}.glb`);
   return <primitive object={scene} />;
 }
 
-export default function RoverModel() {
+export default function RoverModel({ name, baseUrl }: Props) {
   return (
     <div className="w-full h-[500px] rounded-lg shadow overflow-hidden">
       <Canvas>
@@ -16,7 +19,7 @@ export default function RoverModel() {
         <directionalLight position={[5, 5, 5]} intensity={1} />
 
         <Bounds fit clip observe margin={1.2}>
-          <Model />
+          <Model name={name} baseUrl={baseUrl} />
         </Bounds>
 
         <OrbitControls makeDefault />
